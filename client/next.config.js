8f14a4de338d4aa00ab239503d3f927b0a89181f
coldnanee <pmihaylov036@gmail.com>
2023-12-09 07:47:44 +0300

    fix api url bugs

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,10 @@
+require('dotenv').config({ path: "./config/.env" })
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: "http://localhost:4000/api"
+        API_URL: process.env.STATUS && process.env.STATUS === "production" ? `${process.env.API_VPS_URL}/api` : `${process.env.API_LOCAL_URL}/api`,
+        SOCKET_URL: process.env.STATUS && process.env.STATUS === "production" ? process.env.API_VPS_URL : process.env.API_LOCAL_URL
     },
     images: {
         remotePatterns: [
