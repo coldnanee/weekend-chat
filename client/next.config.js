262a15906cd266a634c61a0f84c453f66b6112e5
coldnanee <pmihaylov036@gmail.com>
2023-12-09 18:22:35 +0300

    fix bugs

--- client/next.config.js
+++ client/next.config.js
@@ -3,8 +3,8 @@ require('dotenv').config({ path: "./config/.env" })
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: process.env.STATUS && process.env.STATUS === "production" ? `${process.env.API_VPS_URL}/api` : `${process.env.API_LOCAL_URL}/api`,
-        SOCKET_URL: process.env.STATUS && process.env.STATUS === "production" ? process.env.API_VPS_URL : process.env.API_LOCAL_URL
+        API_URL: `${process.env.SERVER_URL}/api`,
+        SOCKET_URL: process.env.SERVER_URL
     },
     images: {
         remotePatterns: [
