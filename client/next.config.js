4ea640a09336c05f9e6a2fe31c0980cd73c31c13
coldnanee <pmihaylov036@gmail.com>
2023-12-09 20:40:33 +0300

    fix .env bugs

--- client/next.config.js
+++ client/next.config.js
@@ -3,8 +3,8 @@ require('dotenv').config({ path: "./config/.env" })
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: process.env.API_URL,
-        SERVER_URL: process.env.SERVER_URL
+        API_URL: global.process.env.API_URL,
+        SERVER_URL: global.process.env.SERVER_URL
     },
     images: {
         remotePatterns: [
