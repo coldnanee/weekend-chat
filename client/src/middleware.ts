38a22036d8d0af9a4445133cf7317207a398c075
coldnanee <pmihaylov036@gmail.com>
2023-12-27 07:15:34 +0300

    fix middleware bug

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,9 +5,7 @@ const privateRoutesArr = ["/settings", "/", "/support", "/chat"];
 const publicRoutesArr = ["/login", "/registration", "/login/forgot"];
 
 export default function middleware(req: NextRequest) {
-	const { pathname } = req.nextUrl;
-
-	// const pathname = "/" + req.nextUrl.pathname.substring(1).split("/")[0];
+	const pathname = "/" + req.nextUrl.pathname.substring(1).split("/")[0];
 
 	const response = NextResponse.next();
 
