ad3dbca1756522c7d12030568e55539e847247d1
coldnanee <pmihaylov036@gmail.com>
2024-01-03 17:29:58 +0300

    edit build config

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "jest";
 const config: Config = {
 	testEnvironment: "node",
 	preset: "ts-jest",
-	clearMocks: true
+	clearMocks: true,
+	setupFiles: ["./src/libs/setup-env.ts"]
 };
 
 export default config;
